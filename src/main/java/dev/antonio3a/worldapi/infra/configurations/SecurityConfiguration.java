709d97bf45e4f98eaa82698a663b89594878644b
António António <antonioandroid3a@gmail.com>
2024-05-20 22:21:04 +0100

    configure security exceptions handling

--- src/main/java/dev/antonio3a/worldapi/infra/configurations/SecurityConfiguration.java
+++ src/main/java/dev/antonio3a/worldapi/infra/configurations/SecurityConfiguration.java
@@ -4,6 +4,7 @@ import dev.antonio3a.worldapi.infra.util.MyJwtAuthenticationConverter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -23,25 +24,29 @@ public class SecurityConfiguration {
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
         return httpSecurity
                 .csrf(AbstractHttpConfigurer::disable)
-                .authorizeHttpRequests(authorizeRequests ->
+                .exceptionHandling(exceptionHandling ->
+                        exceptionHandling.authenticationEntryPoint(
+                                (request, response, authException) ->
+                                        response.sendError(HttpStatus.UNAUTHORIZED.value(), authException.getMessage())
+                        ).accessDeniedHandler(
+                                (request, response, accessDeniedException) ->
+                                        response.sendError(HttpStatus.FORBIDDEN.value(), accessDeniedException.getMessage())
+                        )
+                ).authorizeHttpRequests(authorizeRequests ->
                         authorizeRequests
                                 .requestMatchers("/world/api/**")
                                 .authenticated()
-                )
-                .authorizeHttpRequests(authorizeRequests ->
+                ).authorizeHttpRequests(authorizeRequests ->
                         authorizeRequests
                                 .anyRequest()
                                 .permitAll()
-                )
-                .oauth2ResourceServer(oauth2ResourceServer ->
+                ).oauth2ResourceServer(oauth2ResourceServer ->
                         oauth2ResourceServer.jwt(jwtConfigurer ->
                                 jwtConfigurer.jwtAuthenticationConverter(myJwtAuthenticationConverter)
                         )
-                )
-                .sessionManagement(sessionManagement ->
+                ).sessionManagement(sessionManagement ->
                         sessionManagement
                                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-                )
-                .build();
+                ).build();
     }
 }
