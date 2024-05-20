6b2033925cb937572d68aa30b9558ba8655a8913
António António <antonioandroid3a@gmail.com>
2024-05-20 22:05:27 +0100

    apply role based access in city controller

--- src/main/java/dev/antonio3a/worldapi/api/controllers/CityController.java
+++ src/main/java/dev/antonio3a/worldapi/api/controllers/CityController.java
@@ -5,6 +5,7 @@ import dev.antonio3a.worldapi.domain.services.CityService;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -15,12 +16,14 @@ public class CityController {
 
     private final CityService cityService;
 
+    @PreAuthorize("hasRole('USER')")
     @GetMapping("/{id}")
     public City getCityById(@PathVariable Integer id) {
         return cityService.getCityById(id);
     }
 
     @GetMapping
+    @PreAuthorize("hasRole('ADMIN')")
     public Page<City> getCities(@RequestParam(defaultValue = "0") Integer page,
                                 @RequestParam(defaultValue = "10") Integer size,
                                 @RequestParam(defaultValue = "ASC") String sortDirection,
