a3c3726c42db56642afa0219749fe212f39a4476
António António <antonioandroid3a@gmail.com>
2024-05-22 10:08:00 +0100

    Enhance WorldApiApplicationTests to verify ApplicationContext loading and bean count

--- src/test/java/dev/antonio3a/worldapi/WorldApiApplicationTests.java
+++ src/test/java/dev/antonio3a/worldapi/WorldApiApplicationTests.java
@@ -1,13 +1,22 @@
 package dev.antonio3a.worldapi;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 class WorldApiApplicationTests {
 
+    @Autowired
+    private ApplicationContext applicationContext;
+
     @Test
     void contextLoads() {
+        assertThat(applicationContext).isNotNull();
+        assertThat(applicationContext.getBeanDefinitionCount()).isPositive();
     }
 
 }
