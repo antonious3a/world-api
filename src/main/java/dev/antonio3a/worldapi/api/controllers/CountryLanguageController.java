5d204770489b1ccb0c62bc8b39956c402e05747b
António António <antonioandroid3a@gmail.com>
2024-06-15 14:25:40 +0100

    App endpoint to CountryLanguageController

--- src/main/java/dev/antonio3a/worldapi/api/controllers/CountryLanguageController.java
+++ src/main/java/dev/antonio3a/worldapi/api/controllers/CountryLanguageController.java
@@ -1,11 +1,21 @@
 package dev.antonio3a.worldapi.api.controllers;
 
+import dev.antonio3a.worldapi.domain.entities.CountryLanguage;
 import dev.antonio3a.worldapi.domain.services.CountryLanguageService;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.security.SecurityRequirements;
 import lombok.RequiredArgsConstructor;
+import org.springdoc.core.annotations.ParameterObject;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.SortDefault;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -17,4 +27,15 @@ import org.springframework.web.bind.annotation.RestController;
 public class CountryLanguageController {
 
     private final CountryLanguageService countryLanguageService;
+
+    @GetMapping(value = "/{countryId}", produces = "application/json")
+    public List<CountryLanguage> getCountryLanguageByCountryId(@PathVariable String countryId) {
+        return countryLanguageService.getCountryLanguageByCountryId(countryId);
+    }
+
+    @GetMapping(produces = "application/json")
+    @PreAuthorize("hasRole('ADMIN')")
+    public Page<CountryLanguage> getCountryLanguages(@ParameterObject @SortDefault(value = "countryCode,asc") Pageable pageable) {
+        return countryLanguageService.getCountryLanguages(pageable);
+    }
 }
--- src/main/java/dev/antonio3a/worldapi/domain/repositories/CountryLanguageRepository.java
+++ src/main/java/dev/antonio3a/worldapi/domain/repositories/CountryLanguageRepository.java
@@ -4,5 +4,9 @@ import dev.antonio3a.worldapi.domain.entities.CountryLanguage;
 import dev.antonio3a.worldapi.domain.entities.CountryLanguageId;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface CountryLanguageRepository extends JpaRepository<CountryLanguage, CountryLanguageId> {
+
+    List<CountryLanguage> findByIdCountryCode(String countryCode);
 }
