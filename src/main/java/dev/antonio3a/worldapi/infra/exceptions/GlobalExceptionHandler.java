410e17143fdb26802aac114d1dbd4f3be977ccae
António António <antonioandroid3a@gmail.com>
2024-06-15 14:32:44 +0100

    Update GlobalExceptionHandler

--- src/main/java/dev/antonio3a/worldapi/infra/exceptions/GlobalExceptionHandler.java
+++ src/main/java/dev/antonio3a/worldapi/infra/exceptions/GlobalExceptionHandler.java
@@ -8,7 +8,6 @@ import org.slf4j.LoggerFactory;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.data.mapping.PropertyReferenceException;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.core.AuthenticationException;
@@ -16,6 +15,7 @@ import org.springframework.validation.method.ParameterErrors;
 import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.method.annotation.HandlerMethodValidationException;
@@ -34,10 +34,9 @@ public class GlobalExceptionHandler {
             NoSuchElementException.class,
             NoHandlerFoundException.class,
     })
-    public ResponseEntity<ErrorResponse> handleNotFoundException(Exception ex, WebRequest request) {
-        return ResponseEntity
-                .status(HttpStatus.NOT_FOUND)
-                .body(new ErrorResponse(HttpStatus.NOT_FOUND, List.of(ex.getMessage()), request.getDescription(false)));
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public ErrorResponse handleNotFoundException(Exception ex, WebRequest request) {
+        return new ErrorResponse(HttpStatus.NOT_FOUND, List.of(ex.getMessage()), request.getDescription(false));
     }
 
     @ExceptionHandler({
@@ -61,7 +60,8 @@ public class GlobalExceptionHandler {
                     )
             )
     )
-    public ResponseEntity<ErrorResponse> handleBadRequestException(Exception ex, WebRequest request) {
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErrorResponse handleBadRequestException(Exception ex, WebRequest request) {
         List<String> messages = switch (ex) {
             case MethodArgumentNotValidException maex -> maex.getFieldErrors()
                     .stream()
@@ -86,36 +86,39 @@ public class GlobalExceptionHandler {
                     .toList();
             default -> List.of(ex.getMessage());
         };
-        return ResponseEntity
-                .status(HttpStatus.BAD_REQUEST)
-                .body(new ErrorResponse(HttpStatus.BAD_REQUEST, messages, request.getDescription(false)));
-    }
-
-    @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<ErrorResponse> handleMethodArgumentNotValidException(MethodArgumentNotValidException ex, WebRequest request) {
-        return ResponseEntity
-                .status(HttpStatus.BAD_REQUEST)
-                .body(new ErrorResponse(HttpStatus.BAD_REQUEST,
-                        ex.getFieldErrors()
-                                .stream()
-                                .map(fieldError -> fieldError.getField() + ": " + fieldError.getDefaultMessage())
-                                .toList(),
-                        request.getDescription(false))
-                );
+        return new ErrorResponse(HttpStatus.BAD_REQUEST, messages, request.getDescription(false));
     }
 
     @ExceptionHandler(AuthenticationException.class)
-    public ResponseEntity<ErrorResponse> handleAuthenticationException(AuthenticationException ex, WebRequest request) {
-        return ResponseEntity
-                .status(HttpStatus.UNAUTHORIZED)
-                .body(new ErrorResponse(HttpStatus.UNAUTHORIZED, List.of(ex.getMessage()), request.getDescription(false)));
+    @ResponseStatus(HttpStatus.UNAUTHORIZED)
+    @ApiResponse(
+            responseCode = "401",
+            description = "Unauthorized",
+            content = @Content(
+                    mediaType = "application/json",
+                    schema = @Schema(
+                            implementation = ErrorResponse.class
+                    )
+            )
+    )
+    public ErrorResponse handleAuthenticationException(AuthenticationException ex, WebRequest request) {
+        return new ErrorResponse(HttpStatus.UNAUTHORIZED, List.of(ex.getMessage()), request.getDescription(false));
     }
 
     @ExceptionHandler(AccessDeniedException.class)
-    public ResponseEntity<ErrorResponse> handleAccessDeniedExceptionException(AccessDeniedException ex, WebRequest request) {
-        return ResponseEntity
-                .status(HttpStatus.FORBIDDEN)
-                .body(new ErrorResponse(HttpStatus.FORBIDDEN, List.of(ex.getMessage()), request.getDescription(false)));
+    @ResponseStatus(HttpStatus.FORBIDDEN)
+    @ApiResponse(
+            responseCode = "403",
+            description = "Forbidden",
+            content = @Content(
+                    mediaType = "application/json",
+                    schema = @Schema(
+                            implementation = ErrorResponse.class
+                    )
+            )
+    )
+    public ErrorResponse handleAccessDeniedExceptionException(AccessDeniedException ex, WebRequest request) {
+        return new ErrorResponse(HttpStatus.FORBIDDEN, List.of(ex.getMessage()), request.getDescription(false));
     }
 
     @ExceptionHandler(Exception.class)
@@ -129,10 +132,9 @@ public class GlobalExceptionHandler {
                     )
             )
     )
-    public ResponseEntity<ErrorResponse> handleException(Exception ex, WebRequest request) {
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public ErrorResponse handleException(Exception ex, WebRequest request) {
         LOGGER.error("Unhandled error (' - ')...", ex);
-        return ResponseEntity
-                .status(HttpStatus.INTERNAL_SERVER_ERROR)
-                .body(new ErrorResponse(HttpStatus.INTERNAL_SERVER_ERROR, List.of(ex.getMessage()), request.getDescription(false)));
+        return new ErrorResponse(HttpStatus.INTERNAL_SERVER_ERROR, List.of(ex.getMessage()), request.getDescription(false));
     }
 }
