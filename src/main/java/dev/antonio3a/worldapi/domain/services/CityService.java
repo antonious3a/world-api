84481d3c93cffb5ce7ab6ad7e4146036c062b0a3
António António <antonioandroid3a@gmail.com>
2024-08-17 21:25:39 +0100

    add self link in get City & Country

--- src/main/java/dev/antonio3a/worldapi/domain/services/CityService.java
+++ src/main/java/dev/antonio3a/worldapi/domain/services/CityService.java
@@ -1,11 +1,15 @@
 package dev.antonio3a.worldapi.domain.services;
 
+import dev.antonio3a.worldapi.api.controllers.CityController;
 import dev.antonio3a.worldapi.domain.entities.City;
 import dev.antonio3a.worldapi.domain.repositories.CityRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
 @Service
 @RequiredArgsConstructor
@@ -14,7 +18,8 @@ public class CityService {
     private final CityRepository cityRepository;
 
     public City getCityById(Integer id) {
-        return cityRepository.findById(id).orElseThrow();
+        return cityRepository.findById(id).orElseThrow()
+                .add(linkTo(methodOn(CityController.class).getCityById(id)).withSelfRel());
     }
 
     public Page<City> getCities(Pageable pageable) {
--- src/main/java/dev/antonio3a/worldapi/domain/services/CountryService.java
+++ src/main/java/dev/antonio3a/worldapi/domain/services/CountryService.java
@@ -1,5 +1,6 @@
 package dev.antonio3a.worldapi.domain.services;
 
+import dev.antonio3a.worldapi.api.controllers.CountryController;
 import dev.antonio3a.worldapi.domain.entities.Country;
 import dev.antonio3a.worldapi.domain.repositories.CountryRepository;
 import lombok.RequiredArgsConstructor;
@@ -7,14 +8,18 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
+
 @Service
 @RequiredArgsConstructor
 public class CountryService {
 
     private final CountryRepository countryRepository;
 
-    public Country getCountryById(String id) {
-        return countryRepository.findById(id).orElseThrow();
+    public Country getCountryByCode(String code) {
+        return countryRepository.findById(code).orElseThrow()
+                .add(linkTo(methodOn(CountryController.class).getCountryByCode(code)).withSelfRel());
     }
 
     public Page<Country> getCountries(Pageable pageable) {
