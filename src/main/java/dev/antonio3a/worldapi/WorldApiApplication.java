4c345d6600ef52e77315bc34946c6e4a4498b108
António António <antonioandroid3a@gmail.com>
2024-08-17 23:33:27 +0100

    enable the rendering as a hypermedia-enabled

--- src/main/java/dev/antonio3a/worldapi/WorldApiApplication.java
+++ src/main/java/dev/antonio3a/worldapi/WorldApiApplication.java
@@ -2,8 +2,10 @@ package dev.antonio3a.worldapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.hateoas.config.EnableHypermediaSupport;
 
 @SpringBootApplication
+@EnableHypermediaSupport(type = EnableHypermediaSupport.HypermediaType.HAL)
 public class WorldApiApplication {
 
     public static void main(String[] args) {
