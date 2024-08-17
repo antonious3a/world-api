d4a146a491e3f413942e34a4b96dd2fd8a20a3e5
António António <antonioandroid3a@gmail.com>
2024-08-18 00:20:04 +0100

    update graphql schema

--- src/main/java/dev/antonio3a/worldapi/api/controllers/WorldController.java
+++ src/main/java/dev/antonio3a/worldapi/api/controllers/WorldController.java
@@ -1,6 +1,8 @@
 package dev.antonio3a.worldapi.api.controllers;
 
+import dev.antonio3a.worldapi.api.payloads.CityDto;
 import dev.antonio3a.worldapi.api.payloads.CountryDto;
+import dev.antonio3a.worldapi.domain.services.CityService;
 import dev.antonio3a.worldapi.domain.services.CountryService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.graphql.data.method.annotation.Argument;
@@ -13,8 +15,15 @@ public class WorldController {
 
     private final CountryService countryService;
 
+    private final CityService cityService;
+
     @QueryMapping
     public CountryDto getCountry(@Argument String code) {
         return countryService.getCountryByCode(code);
+    }
+
+    @QueryMapping
+    public CityDto getCity(@Argument Integer id) {
+        return cityService.getCityById(id);
     }
 }
