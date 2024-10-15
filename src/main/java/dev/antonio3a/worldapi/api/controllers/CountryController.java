ecdba80cc28b0ac5eea0bbfe574840a33c23b7fa
António António <antonioandroid3a@gmail.com>
2024-10-15 17:53:08 +0100

    improve OpenAPI documentation

--- src/main/java/dev/antonio3a/worldapi/api/controllers/CountryController.java
+++ src/main/java/dev/antonio3a/worldapi/api/controllers/CountryController.java
@@ -3,8 +3,16 @@ package dev.antonio3a.worldapi.api.controllers;
 import dev.antonio3a.worldapi.api.payloads.CityDto;
 import dev.antonio3a.worldapi.api.payloads.CountryDto;
 import dev.antonio3a.worldapi.domain.services.CountryService;
+import dev.antonio3a.worldapi.infra.exceptions.ErrorResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.security.SecurityRequirements;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.constraints.Pattern;
 import lombok.RequiredArgsConstructor;
 import org.springdoc.core.annotations.ParameterObject;
@@ -26,12 +34,24 @@ import java.util.List;
         @SecurityRequirement(name = "JWT"),
         @SecurityRequirement(name = "OAUTH2")
 })
+@Tag(name = "Countries", description = "Operations related to countries")
 public class CountryController {
 
     private final CountryService countryService;
 
+    @Operation(summary = "Get a country by its code")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Country found"),
+            @ApiResponse(
+                    responseCode = "404",
+                    description = "Country not found",
+                    content = @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = ErrorResponse.class)
+                    )
+            )
+    })
     @GetMapping(value = "/{code}", produces = "application/json")
-    public CountryDto getCountryByCode(@PathVariable @Pattern(regexp = "[A-Z]{3}") String code) {
+    public CountryDto getCountryByCode(@PathVariable @Pattern(regexp = "[A-Z]{3}") @Parameter(description = "Three letter country code") String code) {
         return countryService.getCountryByCode(code);
     }
 
